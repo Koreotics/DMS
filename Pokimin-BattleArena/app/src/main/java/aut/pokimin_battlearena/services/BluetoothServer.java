f7e1cd8cc4a7c2651f4ee827eda4ae5c323ff9e0
Koreotics <tristan7borja@gmail.com>
2016-05-13 11:52:09 +1200

    version 1.03
    Added Bluetooth classes

---
+++ Pokimin-BattleArena/app/src/main/java/aut/pokimin_battlearena/services/BluetoothServer.java
@@ -0,0 +1,14 @@
+package aut.pokimin_battlearena.services;
+
+/**
+ * @author Tristan Borja (1322097)
+ * @author Dominic Yuen  (1324837)
+ * @author Gierdino Julian Santoso (15894898)
+ */
+public class BluetoothServer implements BluetoothNode {
+
+    @Override
+    public void run() {
+
+    }
+}
