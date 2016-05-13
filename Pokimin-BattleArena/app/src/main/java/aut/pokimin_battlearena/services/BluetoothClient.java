6780e6fbabbdcd590a717d28e4366274e1b9f9fb
Koreotics <tristan7borja@gmail.com>
2016-05-13 13:02:12 +1200

    version 1.04 - Tristan
    Updated Bluetooth

--- Pokimin-BattleArena/app/src/main/java/aut/pokimin_battlearena/services/BluetoothClient.java
+++ Pokimin-BattleArena/app/src/main/java/aut/pokimin_battlearena/services/BluetoothClient.java
@@ -1,5 +1,15 @@
 package aut.pokimin_battlearena.services;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import aut.pokimin_battlearena.activities.BattleActivity;
+
 /**
  * @author Tristan Borja (1322097)
  * @author Dominic Yuen  (1324837)
@@ -7,8 +17,62 @@ package aut.pokimin_battlearena.services;
  */
 public class BluetoothClient implements BluetoothNode {
 
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // FIELDS
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    private boolean stopRequest;
+
+    // bluetooth related fields
+    private BluetoothDevice device;
+    private BluetoothSocket socket;
+
+    // message related fields
+    private List<String> messages;
+    private BattleActivity activity;
+    private BroadcastReceiver receiver;
+
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // CONSTRUCTOR
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    public BluetoothClient() {
+        device   = null;
+        socket   = null;
+        messages = new ArrayList<>();
+        activity = null;
+        receiver = null;
+    }
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // RUNNABLE
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     @Override
     public void run() {
 
+        receiver = new DeviceDiscoverReceiver();
     }
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // UTILITIES
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    @Override
+    public void forward(String message) {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void registerActivity(Activity activity) {
+
+    }
+
+
 }
--- Pokimin-BattleArena/app/src/main/java/aut/pokimin_battlearena/services/BluetoothNode.java
+++ Pokimin-BattleArena/app/src/main/java/aut/pokimin_battlearena/services/BluetoothNode.java
@@ -12,15 +12,24 @@ import java.util.UUID;
  */
 public interface BluetoothNode extends Runnable, Serializable {
 
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // FIELDS
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     // uuid for the Bluetooth application
-    public static final UUID SERVICE_UUID = UUID.fromString("aa7e561f-591f-4767-bf26-e4bff3f0895f");
+    public static final UUID SERVICE_UUID = UUID.fromString("aa7e561f-591f-4767-bf26-e4bff3f0875f");
     // name for the Bluetooth application
     public static final String SERVICE_NAME = "Bluetooth";
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // METHODS
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     // forward a message to all chat nodes in the Bluetooth network
     public void forward(String message);
     // stop this chat node and clean up
     public void stop();
     // registers or unregisters (if null) a ChatActivity for display
-    public void registerActivity(Activity chatActivity);
+    public void registerActivity(Activity activity);
+
 }
